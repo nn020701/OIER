cfb2319e865d054c3473a6ae757dbd4ba70e5549
nn020701 <32233403+nn020701@users.noreply.github.com>
2017-10-03 10:34:08 +0800

    10.2
    去掉了ppt，感觉没必要的说

--- xx/讲课/分治&排序&查找/作业/BZOJ/2141/fuck.bat
+++
@@ -1,7 +0,0 @@
-:loop
-3 > data.in
-2 < data.in > 2.out
-1 < data.in > 1.out
-fc 1.out 2.out
-if errorlevel==1 pause
-goto loop
--- xx/讲课/图论入门/floyd.cpp
+++
@@ -1,5 +0,0 @@
-#define Rep(x,a,b) for (int x=a;x<=b;x++)
-void Floyd(){
-	Rep(i,1,n) Rep(j,1,n) Rep(k,1,n)
-		a[j][k]=min(a[j][k],a[j][i]+a[i][k]);
-}
